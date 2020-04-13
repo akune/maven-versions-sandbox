61549be1dff3a94a633bd28ae6c92b0821104229
Alexander Kune <alexander@kune.de>
2020-04-13 11:19:44 +0200

    chore: something

--- module1/module1-1/src/main/java/de/kune/Test.java
+++ module1/module1-1/src/main/java/de/kune/Test.java
@@ -2,7 +2,7 @@ package de.kune;
 
 public class Test {
 
-    public static void main(String[] args) {
+    public Test() {
         System.out.println("Hello world!");
     }
 
--- module2/src/main/java/de/kune/Main.java
+++ module2/src/main/java/de/kune/Main.java
@@ -3,7 +3,7 @@ package de.kune;
 public class Main {
 
     public static void main(String[] args) {
-        final Test test = new Test();
+        new Test();
     }
 
 }
